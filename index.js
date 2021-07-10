b79876ce74fc51bafbcdfebc4937fa349c44dae9
이예지 <emily4185@douzone.com>
2021-07-10 16:40:26 +0900

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://yeaji:1111@cluster0.8m5w1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
+
+}).then(() => console.log("MongoDB Connected...."))
+  .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World! hihihi')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
